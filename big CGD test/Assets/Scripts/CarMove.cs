b074f3ed4e12bdc6c64096693bae45e880ff03c3
mattgrayy <matt2.gray@live.uwe.ac.uk>
2016-12-23 12:06:30 +0000

    unfinished car stuff

---
+++ big CGD test/Assets/Scripts/CarMove.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMove : MonoBehaviour {
+
+    Rigidbody rb;
+
+    float maxForwardMotion = 10;
+    float forwardMotion = 0;
+
+	// Use this for initialization
+	void Start () {
+        rb = transform.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("0Button1") && forwardMotion < maxForwardMotion)
+        {
+            forwardMotion += 0.03f;
+        }
+
+        if (forwardMotion != 0)
+        {
+            rb.velocity = Vector3.zero;
+            rb.velocity += transform.forward * forwardMotion;
+
+            if (forwardMotion > 0)
+            {
+                forwardMotion -= 0.003f;
+            }
+            else
+            {
+                forwardMotion += 0.003f;
+            }
+
+            if ((forwardMotion < 0.002f && forwardMotion > -0.002f) || (rb.velocity.x + rb.velocity.y + rb.velocity.z < 0.002f && rb.velocity.x - rb.velocity.y - rb.velocity.z > -0.002f))
+            {
+                forwardMotion = 0;
+            }
+        }
+
+        transform.RotateAround(transform.position, transform.up, Input.GetAxis("XAxis1"));
+    }
+}
---
+++ big CGD test/Assets/Scripts/CarTurn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarTurn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void Update()
+    {
+        transform.RotateAround(transform.position, transform.up, Input.GetAxis("XAxis1"));
+    }
+}
